08a73c8a95f8bf0b7a4c357ae8482a2be28b3da8
maryane <maryane.castro993@gmail.com>
2023-09-03 06:29:00 -0300

    Update python.py

--- project/python.py
+++ project/python.py
@@ -1,51 +1,90 @@
-import mediapipe as mp
 import cv2
+import mediapipe as mp
+import time
+#import controller as cnt
+ 
 
-mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
+time.sleep(2.0)
 
-cap = cv2.VideoCapture(0)
+mp_draw=mp.solutions.drawing_utils
+mp_hand=mp.solutions.hands
 
-with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5, max_num_hands=1) as hands:
-    while cap.isOpened():
-        ret, frame = cap.read()
 
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+tipIds=[4,8,12,16,20]
 
-        image = cv2.flip(image, 1)
+video=cv2.VideoCapture(0)
 
-        image.flags.writeable = False
-
-        results = hands.process(image)
-        image.flags.writeable = True
-
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-        num_hands = 0
-
+with mp_hand.Hands(min_detection_confidence=0.5,
+               min_tracking_confidence=0.5) as hands:
+    while True:
+        ret,image=video.read()
+        image=cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image.flags.writeable=False
+        results=hands.process(image)
+        image.flags.writeable=True
+        image=cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        lmList=[]
         if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                fingers_up = [0, 0, 0, 0, 0]
-                for i, finger in enumerate([4, 8, 16, 12, 20]):  
-                    if hand_landmarks.landmark[finger].y < hand_landmarks.landmark[finger - 2].y:
-                        fingers_up[i] = 1
+            for hand_landmark in results.multi_hand_landmarks:
+                myHands=results.multi_hand_landmarks[0]
+                for id, lm in enumerate(myHands.landmark):
+                    h,w,c=image.shape
+                    cx,cy= int(lm.x*w), int(lm.y*h)
+                    lmList.append([id,cx,cy])
+                mp_draw.draw_landmarks(image, hand_landmark, mp_hand.HAND_CONNECTIONS)
+        fingers=[]
+        if len(lmList)!=0:
+            if lmList[tipIds[0]][1] > lmList[tipIds[0]-1][1]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+            for id in range(1,5):
+                if lmList[tipIds[id]][2] < lmList[tipIds[id]-2][2]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+            total=fingers.count(1)
 
-
-                num_fingers_up = sum(fingers_up)
-                
-                cv2.putText(image, f"Numero: {num_fingers_up}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-
-        if results.multi_hand_landmarks:
-            for num, hand in enumerate(results.multi_hand_landmarks):
-                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
-                                          mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
-                                          mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2),
-                                         )
-
-        cv2.imshow('Hand Tracking', image)
-
-        if cv2.waitKey(10) & 0xFF == ord('q'):
+            if total==0:
+                cv2.rectangle(image, (20, 300), (270, 425), (0, 255, 0), cv2.FILLED)
+                cv2.putText(image, "0", (45, 375), cv2.FONT_HERSHEY_SIMPLEX,
+                    2, (255, 0, 0), 5)
+                cv2.putText(image, "LED", (100, 375), cv2.FONT_HERSHEY_SIMPLEX,
+                    2, (255, 0, 0), 5)
+            elif total==1:
+                cv2.rectangle(image, (20, 300), (270, 425), (0, 255, 0), cv2.FILLED)
+                cv2.putText(image, "1", (45, 375), cv2.FONT_HERSHEY_SIMPLEX,
+                    2, (255, 0, 0), 5)
+                cv2.putText(image, "LED", (100, 375), cv2.FONT_HERSHEY_SIMPLEX,
+                    2, (255, 0, 0), 5)
+            elif total==2:
+                cv2.rectangle(image, (20, 300), (270, 425), (0, 255, 0), cv2.FILLED)
+                cv2.putText(image, "2", (45, 375), cv2.FONT_HERSHEY_SIMPLEX,
+                    2, (255, 0, 0), 5)
+                cv2.putText(image, "LED", (100, 375), cv2.FONT_HERSHEY_SIMPLEX,
+                    2, (255, 0, 0), 5)
+            elif total==3:
+                cv2.rectangle(image, (20, 300), (270, 425), (0, 255, 0), cv2.FILLED)
+                cv2.putText(image, "3", (45, 375), cv2.FONT_HERSHEY_SIMPLEX,
+                    2, (255, 0, 0), 5)
+                cv2.putText(image, "LED", (100, 375), cv2.FONT_HERSHEY_SIMPLEX,
+                    2, (255, 0, 0), 5)
+            elif total==4:
+                cv2.rectangle(image, (20, 300), (270, 425), (0, 255, 0), cv2.FILLED)
+                cv2.putText(image, "4", (45, 375), cv2.FONT_HERSHEY_SIMPLEX,
+                    2, (255, 0, 0), 5)
+                cv2.putText(image, "LED", (100, 375), cv2.FONT_HERSHEY_SIMPLEX,
+                    2, (255, 0, 0), 5)
+            elif total==5:
+                cv2.rectangle(image, (20, 300), (270, 425), (0, 255, 0), cv2.FILLED)
+                cv2.putText(image, "5", (45, 375), cv2.FONT_HERSHEY_SIMPLEX,
+                    2, (255, 0, 0), 5)
+                cv2.putText(image, "LED", (100, 375), cv2.FONT_HERSHEY_SIMPLEX,
+                    2, (255, 0, 0), 5)
+        cv2.imshow("Frame",image)
+        k=cv2.waitKey(1)
+        if k==ord('q'):
             break
-
-cap.release()
+video.release()
 cv2.destroyAllWindows()
+
